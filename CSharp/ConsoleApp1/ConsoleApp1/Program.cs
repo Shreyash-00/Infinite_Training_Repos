31233af0cec04819496fc6cff88ca18afe234818
shreyashs <shreyashs@DESKTOP-RQFUPS3>
2024-06-13 02:15:53 -0700

    Shreyash_Dot_Net_Day_01

---
+++ CSharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your first name");
+			string Lname = Console.ReadLine();
+			Console.WriteLine("Please enter your last name");
+			string Rname = Console.ReadLine();
+			Console.WriteLine("Your first name is " +Lname+ " and your last name is " + Rname);
+			Console.ReadLine();
+		}
+	}
+}
