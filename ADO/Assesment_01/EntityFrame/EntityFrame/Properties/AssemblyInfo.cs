afb50d099edc939714061e9713476697e7e37ff1
shreyashs <shreyashs@DESKTOP-RQFUPS3>
2024-07-27 13:09:21 +0530

    LINQ_ASSIGNMENT

---
+++ ADO/Assesment_01/EntityFrame/EntityFrame/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EntityFrame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EntityFrame")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad869c4d-2d5b-411d-9aea-39c8d83a625b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ADO/Assesment_01/Entity_Framework/Entity_Framework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    class Program
+    {
+        //let us create an object of the context class
+        static infiniteDBEntities db = new infiniteDBEntities();
+        static void Main(string[] args)
+        {
+            var emp = db.EMPs.ToList();
+
+            foreach (var e in emp)
+            {
+                Console.WriteLine(e.EMPNO + " " + e.ENAME + " " + e.SAL);
+            }
+
+            Console.Read();
+        }
+    }
+}
+
